eb75369896c8ccbdbbd18c55eb0dbaf1461c87c8
silvansky <v.silvansky@gmail.com>
2012-06-26 17:02:51 +0400

    local player, player info, file size

--- gen.sh
+++ gen.sh
@@ -13,16 +13,17 @@ cd $PUBLIC/$DIR
 cat $CDIR/$HEADER > $FILE
 
 for i in *.$DIR; do
-	echo "<p><a href='$i'>$i</a></p>" >> $FILE
+	SIZE=`du -h "$i" | cut -f 1`
+	echo "<p><a href='$i'>$i</a> ($SIZE)</p>" >> $FILE
 	if [ "$DIR" == "mp3" ]; then
 		LEN=`afinfo -r "$i" | grep duration | cut -d ' ' -f 3 | cut -d '.' -f 1`
 		ENCODED="$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "$i")"
 		echo "file: $i"
 		echo "encoded: $ENCODED"
-		echo "<object type=\"application/x-shockwave-flash\" data=\"http://flv-mp3.com/i/pic/ump3player_500x70.swf\" height=\"30\" width=\"200\"><param name=\"wmode\" 
+		echo "<object type=\"application/x-shockwave-flash\" data=\"player.swf\" height=\"30\" width=\"200\"><param name=\"wmode\" 
 		value=\"transparent\" /><param name=\"allowFullScreen\" value=\"true\" /><param name=\"allowScriptAccess\" value=\"always\" />
 		<param name=\"movie\" value=\"http://flv-mp3.com/i/pic/ump3player_500x70.swf\" /><param name=\"FlashVars\"
-		value=\"way=${ENCODED}&amp;swf=http://flv-mp3.com/i/pic/ump3player_500x70.swf&amp;w=300&amp;h=45&amp;time_seconds=${LEN}&amp;autoplay=0&amp;q=&amp;skin=grey&amp;volume=90&amp;comment=\" />
+		value=\"way=${ENCODED}&amp;swf=http://flv-mp3.com/i/pic/ump3player_500x70.swf&amp;w=300&amp;h=45&amp;time_seconds=${LEN}&amp;autoplay=0&amp;q=&amp;skin=grey&amp;volume=10amp;comment=\" />
 		</object>" >> $FILE
 	fi;
 done
